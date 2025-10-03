b93bc1704be902d586c7816fe4771a00669a1e19
VladCR777 <235779393+pandacloud3@users.noreply.github.com>
2025-10-03 11:27:12 +0300

    add docstring for swagger for Meteostation

--- my_project/auth/route/orders/meteostation_route.py
+++ my_project/auth/route/orders/meteostation_route.py
@@ -9,8 +9,37 @@ meteostation_bp = Blueprint('meteostations', __name__, url_prefix='/meteostation
 @meteostation_bp.get('')
 def get_all_meteostations() -> Response:
     """
-    Gets all meteostations from the database.
-    :return: Response object
+    Get all meteostations
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - name: page
+        in: query
+        type: integer
+        required: false
+        default: 1
+        description: The page number
+    responses:
+      200:
+        description: A list of meteostations
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              id:
+                type: integer
+              name:
+                type: string
+              location_id:
+                type: integer
+              producer_id:
+                type: integer
+              installation_date:
+                type: string
+              interval_id:
+                type: integer
     """
     stations = meteostation_controller.find_all()
     stations_dto = [station.put_into_dto() for station in stations]
@@ -20,9 +49,33 @@ def get_all_meteostations() -> Response:
 @meteostation_bp.post('')
 def create_meteostation() -> Response:
     """
-    Creates a new meteostation in the database.
-    :return: Response object
+    Create a new meteostation
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            name:
+              type: string
+            location_id:
+              type: integer
+            producer_id:
+              type: integer
+            installation_date:
+              type: string
+              format: date
+            interval_id:
+              type: integer
+    responses:
+      201:
+        description: Meteostation created
     """
+
     content = request.get_json()
     station = Meteostation.create_from_dto(content)
     meteostation_controller.create(station)
@@ -32,9 +85,21 @@ def create_meteostation() -> Response:
 @meteostation_bp.get('/<int:station_id>')
 def get_meteostation(station_id: int) -> Response:
     """
-    Gets meteostation by ID.
-    :param station_id: ID of the meteostation
-    :return: Response object
+    Get a meteostation by ID
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - name: station_id
+        in: path
+        type: integer
+        required: true
+        description: ID of the meteostation
+    responses:
+      200:
+        description: Meteostation found
+      404:
+        description: Meteostation not found
     """
     station = meteostation_controller.find_by_id(station_id)
     if station:
@@ -45,9 +110,24 @@ def get_meteostation(station_id: int) -> Response:
 @meteostation_bp.put('/<int:station_id>')
 def update_meteostation(station_id: int) -> Response:
     """
-    Updates meteostation by ID.
-    :param station_id: ID of the meteostation
-    :return: Response object
+    Update a meteostation by ID
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - name: station_id
+        in: path
+        type: integer
+        required: true
+        description: ID of the meteostation
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+    responses:
+      200:
+        description: Meteostation updated
     """
     content = request.get_json()
     station = Meteostation.create_from_dto(content)
@@ -58,9 +138,19 @@ def update_meteostation(station_id: int) -> Response:
 @meteostation_bp.delete('/<int:station_id>')
 def delete_meteostation(station_id: int) -> Response:
     """
-    Deletes meteostation by ID.
-    :param station_id: ID of the meteostation
-    :return: Response object
+    Delete a meteostation by ID
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - name: station_id
+        in: path
+        type: integer
+        required: true
+        description: ID of the meteostation
+    responses:
+      204:
+        description: Meteostation deleted
     """
     meteostation_controller.delete(station_id)
     return make_response("Meteostation deleted", HTTPStatus.NO_CONTENT)
@@ -69,9 +159,19 @@ def delete_meteostation(station_id: int) -> Response:
 @meteostation_bp.get('/name/<string:name>')
 def get_meteostations_by_name(name: str) -> Response:
     """
-    Gets meteostations by name.
-    :param name: Meteostation name
-    :return: Response object
+    Get meteostations by name
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - name: name
+        in: pat
+        type: string
+        required: true
+        description: Meteostation name
+    responses:
+      200:
+        description: List of meteostations with the given name
     """
     stations = meteostation_controller.get_stations_by_name(name)
     return make_response(jsonify([station.put_into_dto() for station in stations]), HTTPStatus.OK)
@@ -80,9 +180,19 @@ def get_meteostations_by_name(name: str) -> Response:
 @meteostation_bp.get('/producer/<int:producer_id>')
 def get_meteostations_by_producer(producer_id: int) -> Response:
     """
-    Gets meteostations by producer ID.
-    :param producer_id: Producer ID
-    :return: Response object
+    Get meteostations by producer ID
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - name: producer_id
+        in: path
+        type: integer
+        required: true
+        description: Producer ID
+    responses:
+      200:
+        description: List of meteostations for the producer
     """
     stations = meteostation_controller.get_stations_by_producer(producer_id)
     return make_response(jsonify([station.put_into_dto() for station in stations]), HTTPStatus.OK)
@@ -91,9 +201,19 @@ def get_meteostations_by_producer(producer_id: int) -> Response:
 @meteostation_bp.get('/interval/<int:interval_id>')
 def get_meteostations_by_interval(interval_id: int) -> Response:
     """
-    Gets meteostations by interval ID.
-    :param interval_id: Interval type ID
-    :return: Response object
+    Get meteostations by interval ID
+    ---
+    tags:
+      - Meteostations
+    parameters:
+      - name: interval_id
+        in: path
+        type: integer
+        required: true
+        description: Interval type ID
+    responses:
+      200:
+        description: List of meteostations with given interval
     """
     stations = meteostation_controller.get_stations_by_interval(interval_id)
     return make_response(jsonify([station.put_into_dto() for station in stations]), HTTPStatus.OK)
