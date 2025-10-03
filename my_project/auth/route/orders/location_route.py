b53c1da14076d7a15234672fc514e9517fc7f9fa
VladCR777 <235779393+pandacloud3@users.noreply.github.com>
2025-10-03 12:01:38 +0300

    try new location_route

--- my_project/auth/route/orders/location_route.py
+++ my_project/auth/route/orders/location_route.py
@@ -9,8 +9,24 @@ location_bp = Blueprint('locations', __name__, url_prefix='/locations')
 @location_bp.get('')
 def get_all_locations() -> Response:
     """
-    Gets all locations from the database.
-    :return: Response object
+    Get all locations
+    ---
+    tags:
+      - Locations
+    responses:
+      200:
+        description: A list of all locations
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              id:
+                type: integer
+              location:
+                type: string
+              stat:
+                type: string
     """
     locations_dto = location_controller.find_all()  # вже DTO
     return make_response(jsonify(locations_dto), HTTPStatus.OK)
@@ -19,8 +35,24 @@ def get_all_locations() -> Response:
 @location_bp.post('')
 def create_location() -> Response:
     """
-    Creates a new location in the database.
-    :return: Response object
+    Create a new location
+    ---
+    tags:
+      - Locations
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            location:
+              type: string
+            stat:
+              type: string
+    responses:
+      201:
+        description: Location created
     """
     content = request.get_json()
     loc = Location.create_from_dto(content)
@@ -31,9 +63,21 @@ def create_location() -> Response:
 @location_bp.get('/<int:location_id>')
 def get_location(location_id: int) -> Response:
     """
-    Gets location by ID.
-    :param location_id: ID of the location
-    :return: Response object
+    Get a location by ID
+    ---
+    tags:
+      - Locations
+    parameters:
+      - name: location_id
+        in: path
+        type: integer
+        required: true
+        description: ID of the location
+    responses:
+      200:
+        description: Location found
+      404:
+        description: Location not found
     """
     loc = location_controller.find_by_id(location_id)
     if loc:
@@ -44,9 +88,29 @@ def get_location(location_id: int) -> Response:
 @location_bp.put('/<int:location_id>')
 def update_location(location_id: int) -> Response:
     """
-    Updates location by ID.
-    :param location_id: ID of the location
-    :return: Response object
+    Update a location by ID
+    ---
+    tags:
+      - Locations
+    parameters:
+      - name: location_id
+        in: path
+        type: integer
+        required: true
+        description: ID of the location
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            location:
+              type: string
+            stat:
+              type: string
+    responses:
+      200:
+        description: Location updated
     """
     content = request.get_json()
     loc = Location.create_from_dto(content)
@@ -57,9 +121,19 @@ def update_location(location_id: int) -> Response:
 @location_bp.delete('/<int:location_id>')
 def delete_location(location_id: int) -> Response:
     """
-    Deletes location by ID.
-    :param location_id: ID of the location
-    :return: Response object
+    Delete a location by ID
+    ---
+    tags:
+      - Locations
+    parameters:
+      - name: location_id
+        in: path
+        type: integer
+        required: true
+        description: ID of the location
+    responses:
+      204:
+        description: Location deleted
     """
     location_controller.delete(location_id)
     return make_response("Location deleted", HTTPStatus.NO_CONTENT)
@@ -68,9 +142,30 @@ def delete_location(location_id: int) -> Response:
 @location_bp.get('/name/<string:name>')
 def get_locations_by_name(name: str) -> Response:
     """
-    Gets locations by name.
-    :param name: Location name
-    :return: Response object
+    Get locations by name
+    ---
+    tags:
+      - Locations
+    parameters:
+      - name: name
+        in: path
+        type: string
+        required: true
+        description: Location name
+    responses:
+      200:
+        description: List of locations with the given name
+        schema:
+          type: array
+          items:
+            type: object
+            properties:
+              id:
+                type: integer
+              location:
+                type: string
+              stat:
+                type: string
     """
     locations = location_controller.find_by_name(name)
     return make_response(jsonify([loc.put_into_dto() for loc in locations]), HTTPStatus.OK)
