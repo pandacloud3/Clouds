5d951b152d499792c73f61b74f5f4ac99c8eb772
VladCR777 <235779393+pandacloud3@users.noreply.github.com>
2025-10-08 19:57:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,5 @@ app.register_blueprint(location_bp)
 app.register_blueprint(auth_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(debug=True)
