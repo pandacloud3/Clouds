18bfc78feb81ccff17703de3abada19caca3a5a3
VladCR777 <235779393+pandacloud3@users.noreply.github.com>
2025-10-08 23:00:25 +0300

    test2

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ class Config:
     # JWT_HEADER_TYPE = "Bearer"
     # JWT_SECRET_KEY = jwt_secret_key
 
-    DB_USER = "AdminPanda"
+    DB_USER = "admin"
     DB_PASSWORD = db_password
     DB_HOST = db_host
     DB_PORT = 3306
-    DB_NAME = "mymeteoDB"
+    DB_NAME = "database-1"
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
