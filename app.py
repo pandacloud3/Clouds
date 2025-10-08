b0ea21617f0d2c2445ff63341847d6cda8903a64
VladCR777 <235779393+pandacloud3@users.noreply.github.com>
2025-10-08 19:25:44 +0300

    jwt fix again

--- app.py
+++ app.py
@@ -88,7 +88,22 @@ swagger_template = {
     "security": [{"Bearer": []}]
 }
 
-swagger = Swagger(app, template=swagger_template)
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+
+swagger = Swagger(app, template=swagger_template, config=swagger_config)
 
 # 📦 Підключаємо Blueprint-и
 app.register_blueprint(location_bp)
